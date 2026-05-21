be920dfca0394f1b1a6ea8dd5e828f50781e27ec
Junaid Guindo <junaidguindo.202400745@gmail.com>
2026-05-21 23:19:00 +0800

    Add files via upload

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,4 +1,4 @@
-const { supabase } = require('../config/supabase');
+const { supabase, supabaseAdmin } = require('../config/supabase');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 
